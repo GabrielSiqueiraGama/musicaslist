cf0a7b4407bccba8eda6cdb84f94ae7fdf528e0b
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-05-25 15:02:36 -0300

    Start: :tada: first commit

---
+++ src/main/java/com/zhant/musicaslist/MusicaslistApplication.java
@@ -0,0 +1,13 @@
+package com.zhant.musicaslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhant/musicaslist/MusicaslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhant.musicaslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
