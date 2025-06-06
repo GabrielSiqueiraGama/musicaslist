3f945cac424466056d342bd49ef3c4c7ce6767a2
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-06-05 22:50:15 -0300

    feat: :seedling: Adding search by category

--- src/main/java/com/zhant/musicaslist/controllers/MusicListController.java
+++ src/main/java/com/zhant/musicaslist/controllers/MusicListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zhant.musicaslist.dto.MusicListDTO;
+import com.zhant.musicaslist.dto.MusicMinDTO;
 import com.zhant.musicaslist.services.MusicListService;
+import com.zhant.musicaslist.services.MusicService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -16,10 +19,18 @@ public class MusicListController {
 
 	@Autowired
 	private MusicListService musicListService;
+	@Autowired
+	private MusicService musicService;
 	
 	@GetMapping
 	public List<MusicListDTO> findAll(){
 		List<MusicListDTO> allMusics = musicListService.findAll(); 
 		return allMusics;
+	}
+	
+	@GetMapping(value = "/{listId}/musics")
+	public List<MusicMinDTO> findByList(@PathVariable Long listId){
+		List<MusicMinDTO> result = musicService.findByList(listId);
+		return result;
 	}
 }
